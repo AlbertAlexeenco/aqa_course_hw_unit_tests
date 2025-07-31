eb19a331b0a96f86012b71f276e7d5cd639f1695
AlbertAlexeenco <alick1777@gmail.com>
2025-07-31 12:41:24 +0300

    hw-4 done

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,29 @@
   преобразовываясь в number
 
 */
+
+"use strict";
+
+const minAge = 18;
+const maxAge = 60;
+let age = NaN;
+
+if (typeof age === "string"){
+  age = Number(age);
+}
+
+if (typeof age !== "number" || isNaN(age) ){
+  console.log("Incorrect data type");
+}
+
+if(age < minAge){
+  console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+}
+else if(age >= minAge && age < maxAge){
+  console.log("Welcome  !");
+}
+else if(age > maxAge){
+  console.log("Keep calm and look Culture channel");
+} else{
+  console.log("Technical work");
+}
