be82c1882a17db6dadfd5bea99e31e43c5b8be7f
AlbertAlexeenco <alick1777@gmail.com>
2025-08-11 23:29:58 +0300

    hw7-1

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -5,18 +5,34 @@
   - Например: mergeArrays([1,2], [3,4], [5,6]) // [1,2,3,4,5,6]
   - Решить с использованием Spread operator
 */
-function mergeArrays() {
-  // Ваш код
+
+function mergeArrays(...arrays) {
+  return [].concat(...arrays);
 }
+
 /*
   2. Devide by _
     - Написать функцию, которая преобразует любое предложение в вот_Такой_Вот_Вид и возвращает его. 
     - Первое слово должно начинаться с буквы в нижнем регистре, у остальных -  верхнем. 
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
+
 function devideBy(sentence) {
-  // Ваш код
+  const words = sentence.trim().split(" ");
+  console.log(words);
+  const newSentence = [];
+  for(let word of words){
+    if(words.indexOf(word) === 0){
+      newSentence.push(word.trim().toLowerCase());
+    } else {
+      newSentence.push(word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase());
+    }
+  }
+  return newSentence.join("_");
 }
+console.log(devideBy(" I am  super  engineer "));
+
+
 /*
   3. Фибаначчи
     - Напишите функцию fibonacci(n), возвращающую энное число Фибоначчи
@@ -25,8 +41,19 @@ function devideBy(sentence) {
       является суммой двух предыдущих
     - Например fibonacci(8) //21
   */
+
 function fibonacci(n) {
-  // Ваш код
+  const arr = [0, 1]
+  if (typeof n !== "number" || n < 0) throw new Error("Incorrect input");
+  if(n < 3){
+    return arr[n];
+  } else {
+    for (let i = 2; i < n; i++){
+      arr.push(arr[i-1] + arr[i-2]);
+    }
+    return arr[n];
+  }
 }
+console.log(fibonacci(0));
 
 export { mergeArrays, fibonacci, devideBy };
