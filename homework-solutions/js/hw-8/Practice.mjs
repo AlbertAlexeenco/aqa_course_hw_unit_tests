fce607a36b02920695c8f4e5723e7b9f0a928793
AlbertAlexeenco <alick1777@gmail.com>
2025-08-19 00:15:45 +0300

    practice 1-6 tasks

--- homework-solutions/js/hw-8/Practice.mjs
+++ homework-solutions/js/hw-8/Practice.mjs
@@ -1,6 +1,6 @@
 // Начальные данные
 
-import { isReturnStatement } from "typescript";
+import { isReturnStatement, reduceEachLeadingCommentRange } from "typescript";
 
 
 
@@ -12,9 +12,43 @@ import { isReturnStatement } from "typescript";
 // Напишите функцию, которая вернёт массив из двух массивов: первый — все положительные числа, второй — все отрицательные (ноль игнорировать).
 // Бонус (+1 балл): решение в одно действие (например, в один reduce).
 
+//const numbers = [5, -2, 8, -7, 3, 0, 12, -4, 9, 6, -5, 11];
+
+function getPosNegArrays (array = []){
+  return array.reduce ((acc, el) => {
+    if (el > 0){
+      acc[0].push(el); 
+    }
+    if( el < 0){
+      acc[1].push(el); 
+    }
+    return acc;
+  },[[], []])
+}
+
+//console.log( getPosNegArrays(numbers));
+
+
 // 2️⃣ sumPosNeg — 1 балл
 // Напишите функцию, которая вернёт массив из двух чисел: сумма положительных и сумма отрицательных.
 // Бонус (+1 балл): использовать результат из задачи 1.
+
+//const numbers = [5, -2, 8, -7, 3, 0, 12, -4, 9, 6, -5, 11];
+
+function sumPosNeg (array = []){
+    // return getPosNegArrays(array).reduce((acc, el, i, arr) => {
+    //   let sum = 0;
+    //   for(const subEl of el){
+    //     sum += subEl;
+    //   }
+    //   acc.push(sum);
+    //   return acc;}
+    // ,[])
+
+    return getPosNegArrays(array).map(arr => arr.reduce((sum, num) => sum + num, 0));
+}
+
+console.log(sumPosNeg(numbers));
 
 // ⸻
 
@@ -24,20 +58,99 @@ import { isReturnStatement } from "typescript";
 // Вернуть массив уникальных длин слов из words, отсортированных по возрастанию.
 // Бонус (+1 балл): без использования Set.
 
+//const words = ['apple', 'banana', 'kiwi', 'Avocado', 'grape', 'pear', 'melon', 'plum', 'Mango'];
+
+function getUniqueSortedLengths(array = []){
+  return array.map(el => el.length)
+  .toSorted((a, b) => a - b)
+  .reduce((acc, el) => {
+     if(!acc.includes(el)){
+      acc.push(el);
+    }
+    return acc;
+  }
+  ,[])
+
+  // return array.reduce((acc, el) => {
+  //   const len = el.length; 
+  //   if(!acc.includes(len)){
+  //     acc.push(len);
+  //   }
+  //   return acc;
+  // }
+  // ,[])
+  // .sort((a, b) => a - b);
+}
+
+console.log(getUniqueSortedLengths(words));
+
+
 // 4️⃣ sumWordsLengthWithoutA — 2 балла
 // Вернуть сумму длин всех слов из words, не содержащих букву “a” или “A”.
 // Бонус (+1 балл): без регулярных выражений.
+
+const words = ['apple', 'banana', 'kiwi', 'Avocado', 'grape', 'pear', 'melon', 'plum', 'Mango'];
+
+function sumWordsLengthWithoutA(array){
+    // return array.filter(el => !(el.toLowerCase().includes("a")))
+    // .map(el => el.length)
+    // .reduce((sum, i) => sum += i, 0);
+
+    // return array.reduce((sum, i) => {
+    //   if(!(el.toLowerCase().includes("a"))){
+    //     sum += i.length;
+    //   }
+    //   return sum;
+    // }, 0);
+
+    return array.reduce((sum, el) => (!el.toLowerCase().includes("a") ? sum += el.length : sum )  ,0);
+}
+
+console.log(sumWordsLengthWithoutA(words));
+
 
 // 5️⃣ secondLargest — 2 балла
 // Вернуть второе по величине число из массива numbers.
 // Бонус (+1 балл): без сортировки.
 
+const numbers = [5, -2, 8, -7, 3, 0, 12, -4, 9, 6, -5, 11];
+
+function secondLargest(array = []) {
+ return array.reduce((biggestNumber, el) => {
+  let secondBiggest;
+  for(const number of array){
+    if(number > biggestNumber) {
+      secondBiggest = biggestNumber;
+      biggestNumber = number;
+    } 
+    if(number < biggestNumber && number > secondBiggest){
+      secondBiggest = number; 
+    }
+  }
+  return biggestNumber;
+ } , -Infinity);
+}
+
+console.log(secondLargest(numbers));
+
 // 6️⃣ getWordsWith — 2 балла
 // Функция принимает array, subString, length. Вернуть все слова из array, которые содержат subString и имеют длину больше или равную length.
 // Бонус (+1 балл): сделать без учёта регистра.
 
-// ⸻
+//const words = ['apple', 'banana', 'kiwi', 'Avocado', 'grape', 'pear', 'melon', 'plum', 'Mango'];
 
+function getWordsWith(array, subString, length){
+  // return array.reduce((acc, el, arr) => {
+  //   if(subString.length <= el.length && el.toLowerCase().includes(subString.toLowerCase())){
+  //     acc.push(el.toLowerCase())
+  //   }
+  //   return acc;
+  // }, [])
+  return array.filter(el => length <= el.length && el.toLowerCase().includes(subString.toLowerCase()));
+}
+
+console.log(getWordsWith(words, "an", 3));
+ 
 // Хард
 
 // 7️⃣ pipelineNumbers — 3 балла
@@ -57,8 +170,8 @@ import { isReturnStatement } from "typescript";
 // 8️⃣ maxProductsListByNames (супер-босс) — 5 баллов
 // Считаем, что положительные числа из numbers — доходы, отрицательные — расходы. После оплаты всех расходов вычислить остаток и вернуть набор названий товаров из products, который можно купить, начиная с самых дешёвых, чтобы общее количество было максимально возможным.
 
-const numbers = [5, -2, 8, -7, 3, 0, 12, -4, 9, 6, -5, 11];
-const words = ['apple', 'banana', 'kiwi', 'Avocado', 'grape', 'pear', 'melon', 'plum', 'Mango'];
+
+//const words = ['apple', 'banana', 'kiwi', 'Avocado', 'grape', 'pear', 'melon', 'plum', 'Mango'];
 const products = [
   ['bread', 12],
   ['milk', 10.5],
@@ -72,39 +185,8 @@ const products = [
   ['yogurt', 1.8]
 ];
 
-// 3️⃣ getUniqueSortedLengths — 2 балла
-// Вернуть массив уникальных длин слов из words, отсортированных по возрастанию.
-// Бонус (+1 балл): без использования Set.
 
 
-// function getUniqueSortedLengths(array){
-//    const wordLength = array.map(el => el.length);
-//    const uniqueLength = wordLength.reduce((res, i) => {
-//     if (!res.includes(i)) {
-//     res.push(i);
-//   }
-//   return res;
-// }, []);
-//     return uniqueLength.toSorted((a,b) => a - b);;
-// }
-
-function getUniqueSortedLengths(array){
-   return array.map(el => el.length)
-   .reduce((res, i) => {
-    if (!res.includes(i)) {
-    res.push(i);
-  }
-  return res;
-}, [])
-.sort((a,b) => a - b);
-}
-
-console.log(getUniqueSortedLengths(words));
-
-
-// 4️⃣ sumWordsLengthWithoutA — 2 балла
-// Вернуть сумму длин всех слов из words, не содержащих букву “a” или “A”.
-// Бонус (+1 балл): без регулярных выражений.
 
 function sumWordsLengthWithoutA(array){
     return array.filter(el => !(el.toLowerCase().includes("a"))).map(el => el.length).reduce((sum, i) => sum += i, 0);
