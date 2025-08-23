2ad595e86b139de2161fbee1743c02af0fcd3ee8
AlbertAlexeenco <alick1777@gmail.com>
2025-08-23 09:56:54 +0300

    hw-10 tasks 1 and 2

--- homework-solutions/js/hw-10/task-2.mjs
+++ homework-solutions/js/hw-10/task-2.mjs
@@ -7,8 +7,19 @@
   Ожидается: { 1: 1, 2: 2, 3: 1, 4: 3, 5: 1 }
 */
 
-function countOccurrences(arr) {
-  // ваш код
+function countOccurrences(arr = []) {
+  return arr.reduce((obj, el) => {
+    if(!(el in obj)){
+      let count = arr.filter(el2 => el2 === el).length;
+      obj[el] = count;
+      return obj;
+    }
+    return obj;
+  }
+ ,{})
 }
+
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
+console.log(countOccurrences(numbers));
 
 export { countOccurrences };
