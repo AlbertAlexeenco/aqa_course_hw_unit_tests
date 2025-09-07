d571e3a6eb9e47ed2084300cefacabc852b633bf
AlbertAlexeenco <alick1777@gmail.com>
2025-09-07 16:08:21 +0300

    hw-12 task 2

--- homework-solutions/js/hw-12/task-2.mjs
+++ homework-solutions/js/hw-12/task-2.mjs
@@ -5,3 +5,24 @@
 //    Проверьте, что айди в респонсе === 201
 //    Функция должна возвращать полученный объект из респонса
 //    Обрабатывайте ошибки с помощью try/cath, в конце выведите в консоль текст, что работа функции завершена
+
+async function createTodo({userId, title, completed}){
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
+            method: "post",
+            body: {
+                userId, title, completed,
+            },
+        })
+        if(response.status !== 201) throw new Error(`Unexpected status code ${response.status}`);
+        const body = await response.json();
+        if(body.id === 201) return body;
+    }
+    catch(error){
+        console.error(error.message);
+    } finally {
+    console.log("Function createTodo finished");
+    }
+}
+
+(createTodo({userId: 1200, title: "Ramam baharam man burum", completed: false})).then(res => console.log(res));
