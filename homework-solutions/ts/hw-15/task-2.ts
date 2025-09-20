038d5b198cff51820ff057197b89fc922fec370a
AlbertAlexeenco <alick1777@gmail.com>
2025-09-20 22:49:09 +0300

    hw-15 3/4 tasks done

--- homework-solutions/ts/hw-15/task-2.ts
+++ homework-solutions/ts/hw-15/task-2.ts
@@ -1,6 +1,49 @@
-// Напишите функцию, реализующую метод массивов map(сам мэп юзать нельзя, надо написать кастомный:). Функция принимеют на вход массив и колбэк. Используйте дженерик типы. 
-//    Затипизировать надо саму функцию и коллбэк.
-//    Создать реализацию функции map, принимающую массив чисел 1-5, возвращающую новый массив, 
-//    где каждый каждый элемент - это элемент исходного массива умноженный на его индекс
-//    Пример:
-//    map([1,2,3,4,5], callback) => [0,2,6,12,20]
+// Напишите дженерик-функцию getKeyByValue, которая принимает объект и значение, и возвращает ключ, 
+// соответствующий этому значению. 
+// Если значение не найдено, функция должна возвращать undefined.
+// Используйте keyof для типизации ключей объекта
+
+const maximka ={
+    name: "Maximka",
+    age: 8,
+    isCute: true
+}
+
+function getKeyByValue<T extends object, U>(obj :T, value: U): keyof T | undefined{
+    for(const key in obj){
+        if(obj[key] === value){
+            return key;
+        }
+    }
+}
+
+console.log(getKeyByValue(maximka, true));
+console.log(getKeyByValue(maximka, 16));
+
+
+//  2. Создайте интерфейс User с полями id (число), username (строка) и email (строка). 
+// Затем создайте интерфейс Admin, который расширяет User, добавляя поле role (строка). 
+// Напишите функцию, которая принимает массив пользователей и возвращает массив администраторов.
+//       [
+//         { id: 1, username: "user1", email: "user1@example.com" },
+//         { id: 2, username: "admin1", email: "admin1@example.com", role: "superadmin" }
+//       ]
+
+
+// interface IUser{
+//     id: number,
+//     username: string,
+//     email: string
+// }
+
+// interface IAdmin extends IUser{
+//     role: string
+// }
+
+// function getAdmins(list: (IUser | IAdmin)[]) : IAdmin[]{
+//     return list.filter(el => isAdmin(el));
+// }
+
+// function isAdmin(user: IUser | IAdmin): user is IAdmin{
+//    return "role" in user;
+// }
