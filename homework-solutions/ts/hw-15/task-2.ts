cdc25dc369eb30fcc1540a28087ec20455876fce
AlbertAlexeenco <alick1777@gmail.com>
2025-09-29 16:11:02 +0300

    hw-15 correction

--- homework-solutions/ts/hw-15/task-2.ts
+++ homework-solutions/ts/hw-15/task-2.ts
@@ -9,7 +9,7 @@ const maximka ={
     isCute: true
 }
 
-function getKeyByValue<T extends object, U>(obj :T, value: U): keyof T | undefined{
+function getKeyByValue<T extends object>(obj :T, value: T[keyof T]): keyof T | undefined{
     for(const key in obj){
         if(obj[key] === value){
             return key;
@@ -19,31 +19,3 @@ function getKeyByValue<T extends object, U>(obj :T, value: U): keyof T | undefin
 
 console.log(getKeyByValue(maximka, true));
 console.log(getKeyByValue(maximka, 16));
-
-
-//  2. Создайте интерфейс User с полями id (число), username (строка) и email (строка). 
-// Затем создайте интерфейс Admin, который расширяет User, добавляя поле role (строка). 
-// Напишите функцию, которая принимает массив пользователей и возвращает массив администраторов.
-//       [
-//         { id: 1, username: "user1", email: "user1@example.com" },
-//         { id: 2, username: "admin1", email: "admin1@example.com", role: "superadmin" }
-//       ]
-
-
-// interface IUser{
-//     id: number,
-//     username: string,
-//     email: string
-// }
-
-// interface IAdmin extends IUser{
-//     role: string
-// }
-
-// function getAdmins(list: (IUser | IAdmin)[]) : IAdmin[]{
-//     return list.filter(el => isAdmin(el));
-// }
-
-// function isAdmin(user: IUser | IAdmin): user is IAdmin{
-//    return "role" in user;
-// }
